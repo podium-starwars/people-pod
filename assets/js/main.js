fa9160ea21173a05103958022ac577b96678eb97
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-02-07 11:54:41 +0530

    chore: fetch data through podium root

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (async () => {
-  const response = await fetch('https://swapi.dev/api/people');
+  const response = await fetch('/api/people');
   const data = await response.json();
   const people = data.results.map(p => p.name).join('\n');
 document.getElementById('people-list').innerHTML = people;
